ecbaa8fcb0215210637f2ab22b8426b57485a846
daniellim051000 <daniellim051000@gmail.com>
2022-04-20 17:24:38 +0800

    install jenkins

--- nutrix_backend/settings.py
+++ nutrix_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'django_jenkins',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#jenkins
+PROJECT_APPS = [
+    'nutrix-backend'
+]
+JENKINS_TASKS = [
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes'
+]
