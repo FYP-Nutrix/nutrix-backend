accef6c26b3412090dd88abd4e124dd2d57db0da
Chui Yee Chong <chuiyee.chong@infrontconsulting.com.my>
2022-06-29 17:38:14 +0800

    nutrition table update

--- nutrition/models.py
+++ nutrition/models.py
@@ -3,18 +3,24 @@ from django.db import models
 from meal.models import MealImage
 
 # Create your models here.
-class NutritionData(models.Model):
+class Nutrition(models.Model):
     nutrition_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
-    nutrition_name = models.CharField(max_length=255)
-    nutrition_unit = models.CharField(max_length=255)
-
-
-class Nutrients(models.Model):
-    nutrient_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
-    nutrient_name = models.CharField(max_length=255)
-    nutrient_value = models.IntegerField()
-    meal_image = models.ForeignKey(MealImage, on_delete=models.CASCADE)
+    meal_name = models.CharField(max_length=255)
+    carb = models.DecimalField(max_digits=7, decimal_places=2)
+    protein = models.DecimalField(max_digits=7, decimal_places=2)
+    fiber = models.DecimalField(max_digits=7, decimal_places=2)
+    fats = models.DecimalField(max_digits=7, decimal_places=2)
+    water = models.DecimalField(max_digits=7, decimal_places=2)
+    mineral = models.DecimalField(max_digits=7, decimal_places=2)
+    vitaminA = models.DecimalField(max_digits=7, decimal_places=2)
+    vitaminB = models.DecimalField(max_digits=7, decimal_places=2)
+    vitaminC = models.DecimalField(max_digits=7, decimal_places=2)
+    vitaminD = models.DecimalField(max_digits=7, decimal_places=2)
+    vitaminE = models.DecimalField(max_digits=7, decimal_places=2)
+    vitaminK = models.DecimalField(max_digits=7, decimal_places=2)
+    calorie_per_serving = models.DecimalField(max_digits=7, decimal_places=3)
 
 class NutritionLogging(models.Model):
     nutrition_logging_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     meal_image = models.ForeignKey(MealImage, on_delete=models.CASCADE)
+    nutrition = models.ForeignKey(Nutrition, on_delete=models.CASCADE)
