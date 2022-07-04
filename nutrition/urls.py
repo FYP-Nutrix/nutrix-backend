23fc46f3412c9e07c9bc2f7c0085db8203df49a2
Chui Yee Chong <chuiyee.chong@infrontconsulting.com.my>
2022-07-04 18:12:11 +0800

    nutrition and meal setting serializer

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -4,6 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('nutrition/', NutritionList.as_view(), name='NutritionList'),
+    path('nutrition/<uuid:pk>/', NutritionDetails.as_view(), name='NutritionDetails'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- user/serializers.py
+++ user/serializers.py
@@ -1,21 +1,21 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from django.contrib.auth import get_user_model
+from user.models import Account, MealSetting
 
-from user.models import Account
+class MealSettingSerializer(serializers.ModelSerializer):
+    account =serializers.CharField(required=False)
 
+    class Meta: 
+        model = MealSetting
+        fields = ['id', 'account', 'daily_calories',]
 
 class UserSerializer(serializers.ModelSerializer):
-
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = Account.objects.create(**validated_data)
-        user.set_password(password)
-        user.save()        
-        return user
+    daily_calories = MealSettingSerializer(required=False)
 
     class Meta:
         model = Account
+        include = ['daily_calories',]
         exclude = ('groups', 'user_permissions',)
         validators = [
             UniqueTogetherValidator(
@@ -23,3 +23,12 @@ class UserSerializer(serializers.ModelSerializer):
                 fields=['email', 'password']
             )
         ]
+
+    def create(self, validated_data):
+        daily_calories = validated_data.pop('daily_calories')
+        user = Account.objects.create(**validated_data)
+        user.set_password(validated_data.get('password'))
+        user.save()
+        if (validated_data.get('is_patient') == True):
+            MealSetting.objects.create(account=user, **daily_calories)
+        return user
