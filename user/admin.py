4466a59054a052682e454aa4291aac87f480dfbf
unknown <daniellim051000@gmail.com>
2022-07-12 22:24:32 +0800

    admin features

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from user.models import Account
+from user.models import Account, MealSetting
 
 # Register your models here.
+# Pending task need to do https://realpython.com/customize-django-admin-python/
 admin.site.register(Account)
+admin.site.register(MealSetting)
