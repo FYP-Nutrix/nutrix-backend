e619a4adcfc6d367ecc40b69dabb13a3a13e2a88
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2022-08-02 07:40:09 +0800

    reset the meal image relation with meal log

--- meal/models.py
+++ meal/models.py
@@ -6,14 +6,6 @@ import uuid
 from user.models import Account
 
 # Create your models here.
-
-# Store Meal Image
-class MealImage(models.Model):
-    image_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, null=False)
-    meal_image = models.ImageField(null=False, upload_to="meal_logging")
-    total_calorie = models.IntegerField()
-    meal_size = models.DecimalField(max_digits=7, decimal_places=2)
-
 # meal type for user to choose for each entry
 MEAL_TYPE = [
     ('BREAKFAST','Breakfast'),
@@ -26,7 +18,6 @@ MEAL_TYPE = [
 # User meal loggin for each meal
 class MealLogging(models.Model):
     meal_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
-    meal_image = models.ForeignKey(MealImage, on_delete=models.CASCADE)
     meal_type = models.CharField(choices=MEAL_TYPE, max_length=20)
     datetime = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
@@ -34,3 +25,11 @@ class MealLogging(models.Model):
     def __str__(self):
         naming = self.user.email + " " + self.meal_type
         return naming
+
+# Store Meal Image
+class MealImage(models.Model):
+    image_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, null=False)
+    meal_image = models.ImageField(null=False, upload_to="meal_logging")
+    total_calorie = models.IntegerField(null=True)
+    meal_size = models.DecimalField(max_digits=7, decimal_places=2,null=True)
+    meal_logging = models.ForeignKey(MealLogging, related_name='meal_image',on_delete=models.CASCADE)
