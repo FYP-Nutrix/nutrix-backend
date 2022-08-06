643e33062bb555959f3f5381c247518b6ed72918
unknown <daniellim051000@gmail.com>
2022-08-06 19:03:04 +0800

    implementation

--- meal/serializers.py
+++ meal/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from meal.api import requestImageName
 from meal.models import MealImage, MealLogging
 
 class MealImageSerializer(serializers.ModelSerializer):
@@ -15,8 +16,14 @@ class MealLogSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         meal_image = validated_data.pop('meal_image')
-        print("Meal Image is here: ", meal_image)
         meal_log = MealLogging.objects.create(**validated_data)
         for meal_image in meal_image:
-            MealImage.objects.create(meal_logging=meal_log, **meal_image)
+            meal_image_data = MealImage.objects.create(meal_logging=meal_log, **meal_image)
+            print("meal image", meal_image['meal_image'])
+            
+            # image_url = meal_image_data.meal_image.url
+            image_url = meal_image['meal_image']
+            print(type(image_url))
+            ai_api_response = requestImageName(image_url)
+            print(ai_api_response)
         return meal_log
