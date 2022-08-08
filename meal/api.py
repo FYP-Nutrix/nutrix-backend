b64a03c9c6ddf18a218b9edf42cc4c0f009ebb28
unknown <daniellim051000@gmail.com>
2022-08-08 18:55:49 +0800

    api calling to get image name

--- meal/api.py
+++ meal/api.py
@@ -1,8 +1,5 @@
-from asyncore import read
-from dataclasses import dataclass
-from email import header
-import json
 import requests
+
 
 def requestImageName(food_image):
     endpoint = "https://nutrifitai-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/f796fbfd-a4dd-473f-918a-2716ed03620c/detect/iterations/Iteration6/image"
@@ -11,11 +8,15 @@ def requestImageName(food_image):
         "Prediction-Key": "9af3ccfe487d4987aaf96fb84f2da07e",
         "Content-Type": "application/octet-stream"
     }
-    payload = {
-        "": (food_image)
-    }
 
-    response = requests.request("POST", endpoint, headers=headers, files=payload)
+    food_image.seek(0)
+    binary_content = food_image.read()
+
+    response = requests.request(
+        "POST", endpoint, headers=headers, data=binary_content)
 
     data = response.json()
-    print(data)
+    food_name = data['predictions'][0]['tagName']
+    print(food_name)
+    
+    return food_name
