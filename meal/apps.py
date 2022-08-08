d9a6e965cea1f6f400281f3d47e31978b0c82c82
unknown <daniellim051000@gmail.com>
2022-08-08 18:56:10 +0800

    django signal

--- meal/apps.py
+++ meal/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MealConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meal'
+
+    def ready(self) -> None:
+        import meal.signal
+        return super().ready()
