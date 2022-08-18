80fc49858f57618aacb607f3b9aeddfb81adad8f
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2022-08-18 19:33:08 +0800

    calculation for total calories for each food

--- meal/signal.py
+++ meal/signal.py
@@ -1,18 +1,8 @@
 from django.db.models.signals import post_save, pre_save
-from meal.models import MealImage, MealLogging
+from meal.models import MealImage
 from django.dispatch import receiver
 
 from nutrition.models import Nutrition, NutritionLogging
-
-# receive meal image then put the meal name
-# @receiver(post_save, sender=MealImage)
-# def add_meal_name(sender, **kwargs):
-#     if sender == MealImage:
-#         print("this code is running")
-#         instance = kwargs['instance']
-#         print(type(instance.meal_image))
-#     else :
-#         print("something is wrong")
 
 @receiver(post_save, sender=MealImage)
 def generate_nutrition(sender, **kwargs):
@@ -22,7 +12,6 @@ def generate_nutrition(sender, **kwargs):
         new_meal_name = instance.meal_name
         try:
             nutrition_info = Nutrition.objects.get(meal_name=new_meal_name)
-            print(nutrition_info.fiber)
             # to get back the previous meal information entry
             meal_image_id = instance.image_id
             meal_image_obj = MealImage.objects.get(image_id=meal_image_id)
@@ -34,3 +23,16 @@ def generate_nutrition(sender, **kwargs):
 
     else :
         print("something is wrong")
+
+@receiver(post_save, sender=NutritionLogging)
+def generate_total_calories(sender, **kwargs):
+    if sender == NutritionLogging:
+        instance = kwargs['instance']
+        nutrition_info = instance.nutrition
+        image = instance.meal_image
+
+        carb = int(nutrition_info.carb)
+        protein = int(nutrition_info.protein)
+        fats = int(nutrition_info.fats)
+        total_calories = (carb*4) + (protein*9) + (fats*4)
+        MealImage.objects.filter(image_id=image.image_id).update(total_calorie=total_calories)
